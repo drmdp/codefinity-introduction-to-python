3b4b5cc7830ef808e74fa7698453cfe20f96b542
drmdp <>
2025-12-02 22:46:08 +0000

    Dictionaries

---
+++ other_data_types/dictionaries/main.py
@@ -0,0 +1,13 @@
+# product ID and category
+grocery_inventory = {
+"Milk": (113, "Dairy"),
+"Eggs": (116, "Dairy"),
+"Bread": (117, "Bakery"),
+"Apples": (141, "Produce")
+}
+bread_details = grocery_inventory.get("Bread")
+print("Bread:", bread_details)
+grocery_inventory.update({"Cookies": (143, "Bakery")})
+print("Inventory after adfding Cookies:", grocery_inventory)
+removed_item = grocery_inventory.pop("Eggs")
+print("Inventory after removing Eggs:", grocery_inventory)
